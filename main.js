41e2eccf1afd85955c1c3c2d502f92e0513350f3
Davoleo <davoleo2001@gmail.com>
2019-12-07 21:54:26 +0100

    Work on accent colors and styling

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-var accentColors = [
-    "ff9933",
-    "ccff33",
-    "00cc00",
-    "00ff99", 
-    "00ccff",
-    "cc66ff",
-    "cc0000",
-    "66ffff",
-]
+let accentColors = [
+    "Gold",
+    "Lime",
+    "Green",
+    "Aqua",
+    "LightBlue",
+    "Pink",
+    "Red",
+    "ClearSky"
+];
+
+let accent = Math.floor(Math.random() * accentColors.length);
+document.body.classList.add("Accent" + accentColors[accent]);
+
