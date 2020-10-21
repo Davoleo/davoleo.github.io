815d3ade98bd3f1c2a5d52c40186fc7dcc2ba5e1
Davoleo <davoleo2001@gmail.com>
2020-10-21 22:12:38 +0200

    Projects Recent Activity

---
+++ js/projects.js
@@ -0,0 +1,27 @@
+GitHubActivity.feed({
+    username: "Davoleo",
+    repository: "Metallurgy-4-Reforged",
+    selector: "#metallurgy_feed",
+    limit: 1
+});
+
+GitHubActivity.feed({
+    username: "Davoleo",
+    repository: "Immersivecraft-MK",
+    selector: "#immcraft_feed",
+    limit: 1
+});
+
+GitHubActivity.feed({
+    username: "Davoleo",
+    repository: "curse-updates",
+    selector: "#curse_updates_feed",
+    limit: 1
+});
+
+GitHubActivity.feed({
+    username: "Davoleo",
+    repository: "BarcodeStock",
+    selector: "#barcode_stock_feed",
+    limit: 1
+});
