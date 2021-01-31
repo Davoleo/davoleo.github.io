a0cdab65cf644cdab312b82bd4b5742ee1c11d8e
Davoleo <davoleo2001@gmail.com>
2021-01-31 16:58:13 +0100

    More dependencies, yay :V

--- src/js/compile.js
+++ src/js/compile.js
@@ -1,7 +1,12 @@
 const header = require("../templates/header.hbs");
+const index = require("../templates/index.hbs");
+const indexData = require("../data/index.json");
 
 Handlebars.registerPartial('header', header);
 
 Handlebars.registerHelper("getCurrentYear", function () {
     return Date.now().getFullYear();
 })
+
+const compiledIndex = Handlebars.compile(index);
+compiledIndex(indexData);
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,43 @@
-//const path = require(path);
+const path = require("path");
+const HandlebarsPlugin = require("handlebars-webpack-plugin");
 
-module.exports = {
+const config =  {
     mode: 'development',
     entry: "./src/js/compile.js",
     output: {
         path: __dirname + "/dist/js",
         filename: "main-bundled.js"
     },
+    plugins: [
+        new HandlebarsPlugin({
+            entry: path.join(__dirname, "src", "templates", "*.hbs"),
+            output: path.join(__dirname, "dist", "[name].html"),
+            
+            data: [
+                path.join(__dirname, "src", "data", "*.json"),
+            ],
+            partials: [
+                path.join(process.cwd(), "src", "templates", "partials", "*.hbs")
+            ],
+            helpers: {
+                nameOfHbsHelper: Function.prototype,
+                projectHelpers: path.join(process.cwd(), "src", "js", "helpers", "*.helper.js")
+            }
+        })
+    ],
     resolve: {
 
     },
     module: {
         rules: [
-            { test: /\.scss$/, loader: "sass-loader" },
-            { test: /\.hbs$/, loader: "handlebars-loader" }
+            { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+            { test: /\.scss$/i, use: "sass-loader" },
+            { test: /\.hbs$/i, use: "handlebars-loader" },
+            { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+            { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource'},
+            { test: /\.json$/i, use: "json-loader" }
         ]
     }
 }
+
+module.exports = config;
